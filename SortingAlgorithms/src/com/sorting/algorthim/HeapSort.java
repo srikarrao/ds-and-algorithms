22fce225a3b092c9b683de4863c5113ce2923c90
Srikar Rao Gandla <srikar.sirks@gmail.com>
2020-03-13 18:35:33 -0700

    Update HeapSort.java

--- SortingAlgorithms/src/com/sorting/algorthim/HeapSort.java
+++ SortingAlgorithms/src/com/sorting/algorthim/HeapSort.java
@@ -1,89 +1,65 @@
 package com.sorting.algorthim;
 
-import java.util.Scanner;
-
+import java.util.Arrays;
 /**
- * 
- * @author SrikarRao :: Java Program to Implement Heap Sort
+ * @author SrikarRao
+ * @Description: Program to implement Heap Sorting
+ * @Input: EG-> 31 21 59 26 41 58 (Unsorted)
+ * @Output: EG-> 21 26 31 41 58 59 (Ascending) 59 58 41 31 26 21 (Descending)
+ * @Time Complexity: O(n^2)
  */
 
 public class HeapSort {
-	private static int N;
 
-	/* Main method */
-	@SuppressWarnings("resource")
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Heap Sort Test\n");
-		int n;
+    public static void main(String[] args) {
+        int[] array = {2, 8, 5, 3, 9, 1};
+        System.out.println("Before Heap Sorting: " + Arrays.toString(array));
+        sort(array);
+        System.out.println("After Heap Sorting: " + Arrays.toString(array));
+    }
 
-		/* Accept number of elements */
-		System.out.println("Enter number of integer elements");
-		n = scan.nextInt();
+    /* Sort Function */
+    public static void sort(int arr[]) {
 
-		/* Make array of n elements */
-		int arrMain[] = new int[n];
+        int n = arr.length - 1;
+        heapify(arr, n);
 
-		/* Accept elements */
-		System.out.println("\nEnter " + n + " integer elements");
-		for (int i = 0; i < n; i++)
-			arrMain[i] = scan.nextInt();
+        for (int i = n; i > 0; i--) {
+            swap(arr, 0, i);
+            n--;
+            maxHeap(arr, n, 0);
+        }
+    }
 
-		/* Call method sort */
-		sort(arrMain);
+    /* Function to build a heap */
+    public static void heapify(int arr[], int size) {
+        for (int i = size / 2; i >= 0; i--) {
+            maxHeap(arr, size, i);
+        }
+    }
 
-		/* Print sorted Array */
-		System.out.println("\nElements after sorting ");
-		for (int j = 0; j < n; j++) {
-			System.out.print(arrMain[j] + " ");
-		}
-		System.out.println();
-	}
+    /* Function to swap largest element in heap */
+    public static void maxHeap(int arr[], int currSize, int largeIndex) {
 
-	/* Sort Function */
-	public static void sort(int arrX[]) {
+        int left = 2 * largeIndex;
+        int right = 2 * largeIndex + 1;
+        int maxIndex = largeIndex;
 
-		/** Find the largest number and place it in the first place of the array **/
-		heapify(arrX);
+        if (left <= currSize && arr[left] > arr[largeIndex])
+            maxIndex = left;
+        if (right <= currSize && arr[right] > arr[maxIndex])
+            maxIndex = right;
 
-		for (int i = N; i > 0; i--) {
-			/** swaps the first element with the ith element **/
-			swap(arrX, 0, i);
-			N = N - 1;
-			maxheap(arrX, 0);
-		}
-	}
+        if (maxIndex != largeIndex) {
+            swap(arr, largeIndex, maxIndex);
+            maxHeap(arr, currSize, maxIndex);
+        }
+    }
 
-	/* Function to build a heap */
-	public static void heapify(int arrY[]) {
-		N = arrY.length - 1;
-		for (int i = N / 2; i >= 0; i--) {
-			maxheap(arrY, i);
-		}
-	}
-
-	/* Function to swap largest element in heap */
-	public static void maxheap(int arrZ[], int i) {
-
-		int left = 2 * i;
-		int right = 2 * i + 1;
-		int max = i;
-
-		if (left <= N && arrZ[left] > arrZ[i])
-			max = left;
-		if (right <= N && arrZ[right] > arrZ[max])
-			max = right;
-
-		if (max != i) {
-			swap(arrZ, i, max);
-			maxheap(arrZ, max);
-		}
-	}
-
-	/* Function to swap two numbers in an array */
-	public static void swap(int arrA[], int i, int j) {
-		int tmp = arrA[i];
-		arrA[i] = arrA[j];
-		arrA[j] = tmp;
-	}
+    /* Function to swap two numbers in an array */
+    public static void swap(int arrA[], int i, int j) {
+        int tmp = arrA[i];
+        arrA[i] = arrA[j];
+        arrA[j] = tmp;
+    }
 }
